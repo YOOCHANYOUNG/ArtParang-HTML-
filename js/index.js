4a942880f2927a26806acca59fd1071f0421c746
DESKTOP-U6DSM6J <yoo4young@gmail.com>
2025-01-06 22:46:47 +0900

    Test by 2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+// 테스트 콘솔
 console.log("TEST-1");
+
+// 로고 클릭 시 메인 페이지로 이동
+document.getElementById("logo").addEventListener("click", function () {
+    // window.location.href = "index.html"; // 메인 페이지의 경로를 지정
+    window.location.href = "https://renameplz.netlify.app"; // 메인 페이지의 경로를 지정
+});
